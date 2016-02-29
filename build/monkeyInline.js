365449e0a912c8487a1ea152b06e8f07e1e89e87
Clyde Smets <clyde.smets@gmail.com>
2016-02-29 16:57:42 +1000

    Moved global functions into it's own js file

--- build/monkeyInline.js
+++ build/monkeyInline.js
@@ -110,6 +110,64 @@ var MonkeyInline = function(){
 
 };
 
+var tools = [
+  {
+    "name" : "monkeyInline-bold",
+    "function_name" : bold,
+    "content" : "<i class='fa fa-bold'></i>",
+    "description" : "Highlight text you wish to make bold"
+  },
+  {
+    "name" : "monkeyInline-italic",
+    "function_name" : italic,
+    "content" : "<i class='fa fa-italic'></i>",
+    "description" : "Highlight text you wish to make italic"
+  },
+  {
+    "name" : "monkeyInline-underline",
+    "function_name" : underline,
+    "content" : "<i class='fa fa-underline'></i>",
+    "description" : "Highlight text you wish to make underline"
+  },
+  {
+    "name" : "monkeyInline-heading1",
+    "function_name" : heading1,
+    "content" : "<i class='fa fa-header'></i>1",
+    "description" : "Highlight text you wish to make a heading 1"
+  },
+  {
+    "name" : "monkeyInline-heading2",
+    "function_name" : heading2,
+    "content" : "<i class='fa fa-header'></i>2",
+    "description" : "Highlight text you wish to make a heading 2"
+  },
+  {
+    "name" : "monkeyInline-heading3",
+    "function_name" : heading3,
+    "content" : "<i class='fa fa-header'></i>3",
+    "description" : "Highlight text you wish to make a heading 3"
+  },
+  {
+    "name" : "monkeyInline-clearFormatting",
+    "function_name" : clearFormatting,
+    "content" : "<i class='fa fa-eraser'></i>",
+    "description" : "Clear formatting"
+  },
+  {
+    "name" : "monkeyInline-link",
+    "function_name" : createLink,
+    "content" : "<i class='fa fa-link'></i>",
+    "description" : "Insert link",
+    "dialog_box" : {
+      "title" : "Insert link",
+      "message" : "Insert the link below to insert it",
+      "width" : "250px",
+      "height" : "200px"
+    }
+  },
+];
+
+
 function create(htmlStr) {
     var frag = document.createDocumentFragment(),
         temp = document.createElement('div');
@@ -209,63 +267,6 @@ function closeDialogBox(element){
   var name = element.id + "-dialog";
   document.getElementById(name).style.display = "none";
 }
-
-var tools = [
-  {
-    "name" : "monkeyInline-bold",
-    "function_name" : bold,
-    "content" : "<i class='fa fa-bold'></i>",
-    "description" : "Highlight text you wish to make bold"
-  },
-  {
-    "name" : "monkeyInline-italic",
-    "function_name" : italic,
-    "content" : "<i class='fa fa-italic'></i>",
-    "description" : "Highlight text you wish to make italic"
-  },
-  {
-    "name" : "monkeyInline-underline",
-    "function_name" : underline,
-    "content" : "<i class='fa fa-underline'></i>",
-    "description" : "Highlight text you wish to make underline"
-  },
-  {
-    "name" : "monkeyInline-heading1",
-    "function_name" : heading1,
-    "content" : "<i class='fa fa-header'></i>1",
-    "description" : "Highlight text you wish to make a heading 1"
-  },
-  {
-    "name" : "monkeyInline-heading2",
-    "function_name" : heading2,
-    "content" : "<i class='fa fa-header'></i>2",
-    "description" : "Highlight text you wish to make a heading 2"
-  },
-  {
-    "name" : "monkeyInline-heading3",
-    "function_name" : heading3,
-    "content" : "<i class='fa fa-header'></i>3",
-    "description" : "Highlight text you wish to make a heading 3"
-  },
-  {
-    "name" : "monkeyInline-clearFormatting",
-    "function_name" : clearFormatting,
-    "content" : "<i class='fa fa-eraser'></i>",
-    "description" : "Clear formatting"
-  },
-  {
-    "name" : "monkeyInline-link",
-    "function_name" : createLink,
-    "content" : "<i class='fa fa-link'></i>",
-    "description" : "Insert link",
-    "dialog_box" : {
-      "title" : "Insert link",
-      "message" : "Insert the link below to insert it",
-      "width" : "250px",
-      "height" : "200px"
-    }
-  },
-];
 
 function bold(){
   var ele = document.getElementById("monkeyInline-bold");
---
+++ js/global_functions.js
@@ -0,0 +1,100 @@
+
+function create(htmlStr) {
+    var frag = document.createDocumentFragment(),
+        temp = document.createElement('div');
+    temp.innerHTML = htmlStr;
+    while (temp.firstChild) {
+        frag.appendChild(temp.firstChild);
+    }
+    return frag;
+}
+
+
+function getClassElement(findClass) {
+    var elems = document.getElementsByTagName('*'), i;
+    var classElems = [];
+    var counter = 0;
+    for (i in elems) {
+        if((' ' + elems[i].className + ' ').indexOf(' ' + findClass + ' ') > -1) {
+            classElems[counter] = elems[i];
+            counter++;
+        }
+    }
+    return classElems;
+}
+
+function insertAfter(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+function highlightButton(element){
+  var li = element.getElementsByTagName("li")[0];
+  li.className = "used";
+}
+function unHightlightButton(element){
+  var li = element.getElementsByTagName("li")[0];
+  li.className = "";
+}
+
+function fadeIn(el) {
+  el.style.opacity = 0;
+
+  var last = +new Date();
+  var tick = function() {
+    el.style.opacity = +el.style.opacity + (new Date() - last) / 400;
+    last = +new Date();
+
+    if (+el.style.opacity < 1) {
+      (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+    }
+  };
+
+  tick();
+}
+
+function createDocumentShadow(){
+  var documentShadow = document.createElement("div");
+  documentShadow.id = "monkeyBackground";
+  document.body.appendChild(documentShadow);
+}
+
+function openDialogBox(element){
+  for(var i = 0; i < tools.length; i++){
+    var name = tools[i].name;
+    if (name == element.id){
+      var dialogName = name+"-dialog";
+      document.getElementById("monkeyBackground").style.display = "block";
+      fadeIn(document.getElementById("monkeyBackground"));
+      document.getElementById(dialogName).style.display = "block";
+      fadeIn(document.getElementById(dialogName));
+      showDialogButtonSubmit(element);
+      break;
+    }
+  }
+}
+
+function showDialogButtonSubmit(element){
+  var name = element.id + "-submit";
+  document.getElementById(name).style.display = "block";
+}
+
+function dialogButtonSubmit(element){
+  var name = element.id + "-submit";
+  var ele = document.getElementById(name);
+  ele.onclick = function(){
+    if (ele.style.display !== "none"){
+      ele.style.display = "none";
+      closeDocumentShadow();
+      closeDialogBox(element);
+    }
+  };
+}
+
+function closeDocumentShadow(){
+  document.getElementById("monkeyBackground").style.display = "none";
+}
+
+function closeDialogBox(element){
+  var name = element.id + "-dialog";
+  document.getElementById(name).style.display = "none";
+}
