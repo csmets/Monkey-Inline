71b40e521a908399dec60a49b24e404e4593ac95
Clyde Smets <clyde.smets@gmail.com>
2016-03-09 14:48:51 +1000

    Removed unnecessary files for gh-pages

--- js/tools/clearFormatting.js
+++
@@ -1,7 +0,0 @@
-function clearFormatting(){
-  var ele = document.getElementById("monkeyInline-clearFormatting");
-  ele.onclick = function(){
-    document.execCommand('removeFormat');
-    document.execCommand('formatBlock',false,'<p>');
-  };
-}
